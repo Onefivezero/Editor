b763fcb048a141cccea1826f8c1638ebe80b2bec
Onefivezero <43936380+Onefivezero@users.noreply.github.com>
2020-07-27 11:21:11 +0300

    Web güncelleme 2
    printx eklendi

--- WEB_API/net.py
+++ WEB_API/net.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,3 +10,12 @@ def intro():
 @app.route('/print/<some_string>')
 def printer(some_string):
 	return "Print: %s" % some_string
+	
+@app.route('/printx')
+def printvar():
+	query_parameters = request.args
+	id = query_parameters.get("id")
+	if id:
+		return id
+	else:
+		return "Nothing"
