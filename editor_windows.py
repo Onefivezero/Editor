c2f6c292684eb23522266c8ae0180d270941b799
Onefivezero <43936380+Onefivezero@users.noreply.github.com>
2020-08-20 13:45:09 +0300

    Chatbox update
    Chatbox yazı kutusu görünümü düzeltildi

--- editor_windows.py
+++ editor_windows.py
@@ -17,13 +17,17 @@ from PyQt5.QtGui import QColor, QFontMetrics
 
 #CHATBOX DESIGN START
 
+io = 0
+altri = 1
+
 USER_ME = 0
 USER_THEM = 1
 
-BUBBLE_COLORS = {USER_ME: "#90caf9", USER_THEM: "#a5d6a7"}
+BUBBLE_COLORS = {io: "#90caf9", altri: "#a5d6a7"}
+USER_TRANSLATE = {io: QPoint(20, 0), altri: QPoint(0, 0)}
 
-BUBBLE_PADDING = QMargins(15, 5, 15, 5)
-TEXT_PADDING = QMargins(25, 15, 25, 15)
+BUBBLE_PADDING = QMargins(15, 5, 35, 5)
+TEXT_PADDING = QMargins(25, 15, 45, 15)
 
 
 class MessageDelegate(QStyledItemDelegate):
@@ -31,13 +35,18 @@ class MessageDelegate(QStyledItemDelegate):
     Draws each message.
     """
 
+    _font = None
+
     def paint(self, painter, option, index):
+        painter.save()
         # Retrieve the user,message uple from our model.data method.
         user, text = index.model().data(index, Qt.DisplayRole)
 
+        trans = USER_TRANSLATE[user]
+        painter.translate(trans)
+
         # option.rect contains our item dimensions. We need to pad it a bit
         # to give us space from the edge to draw our shape.
-
         bubblerect = option.rect.marginsRemoved(BUBBLE_PADDING)
         textrect = option.rect.marginsRemoved(TEXT_PADDING)
 
@@ -49,26 +58,41 @@ class MessageDelegate(QStyledItemDelegate):
         painter.setBrush(color)
         painter.drawRoundedRect(bubblerect, 10, 10)
 
-        # draw the triangle bubble-pointer, starting from
-
+        # draw the triangle bubble-pointer, starting from the top left/right.
         if user == USER_ME:
             p1 = bubblerect.topRight()
         else:
             p1 = bubblerect.topLeft()
         painter.drawPolygon(p1 + QPoint(-20, 0), p1 + QPoint(20, 0), p1 + QPoint(0, 20))
 
+        toption = QTextOption()
+        toption.setWrapMode(QTextOption.WrapAtWordBoundaryOrAnywhere)
+
         # draw the text
-        painter.setPen(Qt.black)
-        painter.drawText(textrect, Qt.TextWordWrap, text)
+        doc = QTextDocument(text)
+        doc.setTextWidth(textrect.width())
+        doc.setDefaultTextOption(toption)
+        doc.setDocumentMargin(0)
+
+        painter.translate(textrect.topLeft())
+        doc.drawContents(painter)
+        painter.restore()
 
     def sizeHint(self, option, index):
         _, text = index.model().data(index, Qt.DisplayRole)
-        # Calculate the dimensions the text will require.
-        metrics = QApplication.fontMetrics()
-        rect = option.rect.marginsRemoved(TEXT_PADDING)
-        rect = metrics.boundingRect(rect, Qt.TextWordWrap, text)
-        rect = rect.marginsAdded(TEXT_PADDING)  # Re add padding for item size.
-        return rect.size()
+        textrect = option.rect.marginsRemoved(TEXT_PADDING)
+
+        toption = QTextOption()
+        toption.setWrapMode(QTextOption.WrapAtWordBoundaryOrAnywhere)
+
+        doc = QTextDocument(text)
+        doc.setTextWidth(textrect.width())
+        doc.setDefaultTextOption(toption)
+        doc.setDocumentMargin(0)
+
+        textrect.setHeight(doc.size().height())
+        textrect = textrect.marginsAdded(TEXT_PADDING)
+        return textrect.size()
 
 
 class MessageModel(QAbstractListModel):
@@ -80,6 +104,9 @@ class MessageModel(QAbstractListModel):
         if role == Qt.DisplayRole:
             # Here we pass the delegate the user, message tuple.
             return self.messages[index.row()]
+
+    def setData(self, index, role, value):
+        self._size[index.row()]
 
     def rowCount(self, index):
         return len(self.messages)
